000bef3cefe49cddcb351440456f7d8a3120c806
Moses Kolawole <mosesolorunfemikolawole@gmail.com>
2026-01-13 17:57:18 +0100

    Add files via upload
    added the project header files

---
+++ Header_File/string_ops.h
@@ -0,0 +1,10 @@
+#ifndef STRING_OPS_H
+#define STRING_OPS_H
+#define N 100
+
+void original_string(char original[N]);
+void reversed_string(char original[N]);
+void string_counter(char original[N]);
+
+
+#endif // STRING_OPS_H
